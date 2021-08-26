bc8b511390c421cd80a7fad485906cbb373c5a7d
milquetoastable <5898731+milquetoastable@users.noreply.github.com>
2021-08-26 12:22:11 +0100

    Adds PowerShell script to create symlink to NeoVim configuration file

---
+++ create-symlinks.ps1
@@ -0,0 +1,7 @@
+function Main {
+    $path = Get-Item -Path .\.vimrc | Select-Object -Expand FullName
+
+    New-Item -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\nvim\init.vim" -Target $path
+}
+
+. Main
