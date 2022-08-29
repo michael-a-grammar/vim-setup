9bb98bc4f1daf643189ff540521ce8a8f3991593
milque <5898731+milquetoastable@users.noreply.github.com>
2022-08-29 05:11:04 +0100

    Fixes

--- lua/plugin-keymappings/telescope/find_files/finderscope.lua
+++ lua/plugin-keymappings/telescope/find_files/finderscope.lua
@@ -1,31 +1,30 @@
-local telescope         = require'telescope.builtin'
-local file_browser      = require'telescope'.extensions.file_browser.file_browser
-local finderscope       = {}
-local file_browser_name = 'file_browser'
+local telescope    = require'telescope.builtin'
+local file_browser = require'telescope'.extensions.file_browser.file_browser
+local finderscope  = {}
 
-finderscope.find_files_using = function(opts) 
+finderscope.find_files_using = function(opts)
   local find_files_within_dir_fn = opts.find_files_within_dir_fn
   local file_types               = opts.file_types
-  local desc_prefix              = opts.desc_prefix or ''
   local desc_suffix              = opts.desc_suffix or ''
-  local result                   = {}
+  local maps                     = {}
 
-  local telescope_fns = {
-    { file_browser_name, 'd', file_browser,          'File browser',     },
-    { 'find_files',      'f', telescope.find_files,  'Find files'        },
-    { 'live_grep',       's', telescope.live_grep,   'Grep'              },
-    { 'grep_string',     'w', telescope.grep_string, 'Grep current word' }
-  }
+  local set_wd = function(key, picker_opts)
+    if find_files_within_dir_fn then
+      picker_opts[key] = find_files_within_dir_fn()
+    end
+    return picker_opts
+  end
 
-  for _, val in pairs(telescope_fns) do
-    local name, key, telescope_fn, desc = unpack(val)
+  local cwd = function(picker_opts)
+    return set_wd('cwd', picker_opts)
+  end
 
-    desc = desc_prefix .. ' ' .. desc .. ' ' .. desc_suffix
+  local path = function(picker_opts)
+    return set_wd('path', picker_opts)
+  end
 
-    local telescope_opts  = {}
-    local is_file_browser = name == file_browser_name
-
-    if file_types and not is_file_browser then
+  local rg = function(picker_opts)
+    if file_types then
       local file_types_joined = ''
 
       local add_file_type = function(file_type)
@@ -40,28 +39,64 @@ finderscope.find_files_using = function(opts)
         add_file_type(file_types)
       end
 
-      telescope_opts.find_command = { 'rg', file_types_joined, '--files' }
+      picker_opts.find_command = { 'rg', file_types_joined, '--files' }
     end
+    return picker_opts
+  end
 
-    result[name] = {
-      key    = key,
+  local pickers = {
+    {
+      name = 'file_browser',
+      key  = 'd',
+      fn   = file_browser,
+      desc = 'File browser',
+      opts = {
+        cwd        = path,
+        file_types = false
+      }
+    },
+    {
+      name = 'find_files',
+      key  = 'f',
+      fn   = telescope.find_files,
+      desc = 'Find files',
+      opts = {
+        cwd        = cwd,
+        file_types = rg
+      },
+    },
+    {
+      name = 'live_grep',
+      key  = 's',
+      fn   = telescope.live_grep,
+      desc = 'Grep',
+      opts = {
+        cwd        = cwd,
+        file_types = rg
+      }
+    }
+    --{ 'grep_string',     'w', telescope.grep_string, 'Grep current word' }
+  }
+
+  for _, picker in ipairs(pickers) do
+    local picker_opts  = {}
+    local desc         = picker.desc .. (desc_suffix and ' ' .. desc_suffix)
+
+    maps[picker.name] = {
+      key    = picker.key,
       desc   = desc,
       action = function()
-        print(vim.bo.filetype)
-        if find_files_within_dir_fn then
-          local cwd_key           =  (is_file_browser and 'path') or 'cwd'
-          telescope_opts[cwd_key] = find_files_within_dir_fn()
+        for _, opt in pairs(picker.opts) do
+          if opt then
+            opt(picker_opts)
+          end
         end
-
-        telescope_opts.additional_args = function(opts)
-          require'milque.neon'.i(opts)
-        end
-        telescope_fn(telescope_opts)
+        picker.fn(picker_opts)
       end
     }
   end
 
-  return result
+  return maps
 end
 
 finderscope.set = function(set_fn, finderscope_fn)
