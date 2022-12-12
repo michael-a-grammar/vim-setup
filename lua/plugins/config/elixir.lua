0679c02033891225aced48b1a46e3df33a1a9e77
mike-a-grammar <5898731+michael-a-grammar@users.noreply.github.com>
2022-12-12 17:15:27 +0000

    Updates

--- lua/plugins/config/elixir.lua
+++ lua/plugins/config/elixir.lua
@@ -3,7 +3,7 @@ return function()
 
   local capabilities =
     require'cmp_nvim_lsp'
-      .update_capabilities(vim.lsp.protocol.make_client_capabilities())
+      .default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
   elixir.setup {
     settings           = elixir.settings {
