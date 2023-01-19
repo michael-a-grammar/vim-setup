d41771618d8a391b08af3f9f82bed0da89ea8026
mike-a-grammar <5898731+michael-a-grammar@users.noreply.github.com>
2023-01-19 15:23:01 +0000

    Fixes

--- lib/elden.ex
+++ lib/elden.ex
@@ -1,33 +1,79 @@
 defmodule Elden do
-  @commands [:install, :sync, :dev]
+  @commands [:dev, :sync, :update]
 
   def main(args) do
     IO.inspect(args)
 
-    case args do
-      [command] -> nil
-      [command | _] -> nil
-      [] -> nil
-    end
+    elden_path = get_env_path("ELDEN_PATH")
+    config_path = get_env_path("XDG_CONFIG_HOME", default: "~/.config", paths: ["nvim"])
 
-    Enum.map(args, fn arg ->
-      arg
-      |> String.to_atom()
-      |> then(fn command ->
-        @commands
-        |> Enum.member?(command)
+    IO.inspect([elden_path, config_path])
+
+    case args do
+      [arg] ->
+        arg
+        |> String.to_atom()
+        |> then(fn command ->
+          @commands
+          |> Enum.member?(command)
+          |> then(fn
+            true ->
+              case command do
+                :sync ->
+                  2
+
+                :dev ->
+                  System.cmd("kitty", ["--session", "elden.dev", "--directory", "#{elden_path}"])
+              end
+
+            false ->
+              {:error, arg}
+          end)
+        end)
+
+      [arg | other_args] ->
+        {:error, arg, other_args}
+
+      [] ->
+        :error
+    end
+  end
+
+  def get_env_path(varname, opts \\ []) do
+    default = Keyword.get(opts, :default)
+    paths = Keyword.get(opts, :paths, [])
+
+    env_path =
+      case System.get_env(varname) do
+        nil ->
+          if default != nil, do: default, else: nil
+
+        env_path ->
+          env_path
+      end
+
+    case env_path do
+      nil ->
+        {:error, "No system env for varname #{varname} found"}
+
+      env_path ->
+        expanded_path =
+          env_path
+          |> Path.expand()
+          |> then(fn expanded_path ->
+            [expanded_path | paths]
+          end)
+          |> Path.join()
+
+        expanded_path
+        |> File.dir?()
         |> then(fn
           true ->
-            {:ok, command}
+            expanded_path
 
           false ->
-            {:error, command}
+            {:error, ""}
         end)
-      end)
-    end)
-    |> Enum.each(fn
-      {:ok, _} -> 2
-      {:error, _} -> 2
-    end)
+    end
   end
 end
