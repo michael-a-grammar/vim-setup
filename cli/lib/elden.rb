56a61da6f852fca0f3b3887e24292d9e5321ebfe
mike-a-grammar <5898731+michael-a-grammar@users.noreply.github.com>
2023-01-23 15:26:31 +0000

    Fixes

---
+++ cli/lib/elden.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative 'elden/version'
+
+module Elden
+  class CLI
+    def start()
+      puts 'hello'
+    end
+  end
+end
--- cli/test/elden_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule EldenTest do
-  use ExUnit.Case
-  doctest Elden
-
-  test "greets the world" do
-    assert Elden.hello() == :world
-  end
-end
