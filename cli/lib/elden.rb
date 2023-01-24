85af5e259fd34e47bebad6f463b34e9a40af6dc8
mike-a-grammar <5898731+michael-a-grammar@users.noreply.github.com>
2023-01-24 09:10:56 +0000

    Updates

--- cli/lib/elden.rb
+++ cli/lib/elden.rb
@@ -1,11 +1,29 @@
 # frozen_string_literal: true
 
-require_relative 'elden/version'
+require_relative "elden/version"
 
 module Elden
   class CLI
-    def start()
-      puts 'hello'
+    @commands = %i[dev sync update]
+
+    def self.start
+      elden_path  = get_env_path("ELDEN_PATH")
+      config_path = get_env_path("XDG_CONFIG_HOME", "~/", ".config", "nvim")
+
+      puts [elden_path, config_path]
+      # `kitty @ launch --type='os-window' nvim`
+    end
+
+    def self.get_env_path(name, default = "", *paths)
+      joined_path =
+        File.join(
+          File.expand_path(
+            ENV[name] || default
+          ),
+          paths
+        )
+
+      File.exist?(joined_path) ? joined_path : false
     end
   end
 end
