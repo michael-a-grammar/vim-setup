0ad71826d0beafc1cb04f6e5d2e0a77ba76e0e9c
mike-a-grammar <5898731+michael-a-grammar@users.noreply.github.com>
2023-05-12 16:42:39 +0100

    Updates

--- nvim/lua/plugins/config/catppuccin.lua
+++ nvim/lua/plugins/config/catppuccin.lua
@@ -7,9 +7,16 @@ return function()
 
     flavour = 'mocha',
 
+    dim_inactive = {
+      enabled    = false,
+      shade      = "dark",
+      percentage = 0.15,
+    },
+
     integrations = {
       cmp        = true,
       fern       = true,
+      neogit     = true,
       notify     = true,
       telescope  = true,
       treesitter = true,
